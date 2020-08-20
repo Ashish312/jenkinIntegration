c456ce564faedef629153e2f74db6d6613df7f8c
ashishFidel@gmail.com <ashishFidel@gmail.com>
2020-08-21 00:40:16 +0530

    Pushing code

---
+++ src/com/main/TestSeleniumScript.java
@@ -0,0 +1,21 @@
+package com.main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestSeleniumScript {
+
+	@Test
+	public void seleniumScript() {
+
+		System.setProperty("webdriver.chrome.driver", "/TestSeleniumProject/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		String baseUrl = "https://www.google.com/";
+		driver.get(baseUrl);
+		
+		System.out.println("The scripts is running....");
+		driver.close();
+	}
+}
